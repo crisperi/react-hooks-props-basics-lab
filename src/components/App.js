4290e232aa2809c578f4b971b25d4a5ce3df676d
coder-w <phoenixedian@gmail.com>
2024-04-27 01:37:17 +0300

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-import Links from "./Links";
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
@@ -14,7 +13,7 @@ let github =user.links.github;
 let color =user.color;
  let bio = user.bio;
 
-function App(props) {
+function App() {
 
   return (
     <div>
--- src/components/Links.js
+++ src/components/Links.js
@@ -5,8 +5,8 @@ function Links(props) {
 console.log(props)
     return <>
         <h3> Links</h3>
-        <a href="{props.github}" >{props.github} </a>
-        <a href="{props.linkedin}">{props.linkedin} </a>
+        <a href={props.github} >{props.github} </a>
+        <a href={props.linkedin}>{props.linkedin} </a>
     </>
 }
 
